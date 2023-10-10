ff7bdf90d6cee8833e580d9199e5e1e69f26aa42
Bhavani Batchu <bhavani.batchu23@gmail.com>
2023-10-10 09:05:45 +0000

    LoginApp

---
+++ src/components/Logout/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Logout = props => {
+  const {logout} = props
+
+  return (
+    <button type="button" className="Logout-button" onClick={logout}>
+      Logout
+    </button>
+  )
+}
+
+export default Logout
